dc78f0e143e9d5b570b4ec27aa401964e334152d
Nicolas Melo <nicolasmelo@Nicolass-MacBook-Pro.local>
2023-05-17 22:27:10 -0300

    better DX with api for creating the api handler, added turbo commands for building and running everything sequentially, also added a develop command, so we can test everything at once

--- packages/api/src/index.ts
+++ packages/api/src/index.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 
 export default function handler(req: VercelRequest, res: VercelResponse) {
-  res.status(200).end('Hello Cron!');
+  res.status(200).end('Hello Fast!');
 }
